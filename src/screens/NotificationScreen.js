3f79d7f04853e20ec5d03a120a25592b3b5b805a
JAZZHAND1 <47551224+JAZZHAND1@users.noreply.github.com>
2020-12-16 23:11:39 +0600

    notifscreen

--- src/screens/NotificationScreen.js
+++ src/screens/NotificationScreen.js
@@ -1,18 +1,52 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState,useEffect } from "react";
+import { View, StyleSheet,FlatList,} from "react-native";
 import { Text, Card, Avatar } from "react-native-elements";
 import HeaderHome from "./../components/HeaderHome";
 import { AuthContext } from "../providers/AuthProvider";
+import * as firebase from "firebase";
+import "firebase/firestore";
 const NotificationScreen = (props) => {
+  const[notify,setnotify] =useState({});
+  let render =[];
+  const loadall = async () => {
+    firebase
+    .firestore()
+    .collection("posts")
+    .onSnapshot((querySnapshot) => {
+      let temp_posts = [];
+      querySnapshot.forEach((doc) => {
+        temp_posts.push({
+         data: doc.data(),
+        });
+      });
+      setnotify(temp_posts);
+     // setLoading(false);
+     // alert(posts);
+      
+    })
+}
+
+useEffect(() => {
+  loadall();
+}, []);
+
+console.log(notify);
+
+
+
+
+  let notification =[];
   return (
     <AuthContext.Consumer>
       {(auth) => (
+        
         <View style={styles.viewStyle}>
           <HeaderHome
             DrawerFunction={() => {
               props.navigation.toggleDrawer();
             }}
           />
+            
           <Card>
             <View style={{ flexDirection: "row", alignItems: "center" }}>
               <Avatar
@@ -29,8 +63,22 @@ const NotificationScreen = (props) => {
                 
                 Pam Beesley Liked Your Post.
               </Text>
+              
+
+
             </View>
           </Card>
+          <FlatList
+            data={notify}
+            renderItem={({ item }) => {
+              return (
+               <Text>
+                  {item.data.likers} has liked your post
+               </Text>
+
+              );
+            }}
+          />
         </View>
       )}
     </AuthContext.Consumer>
